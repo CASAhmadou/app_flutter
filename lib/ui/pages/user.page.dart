d5a70cf7d6195582809221db6b5cbd4402d74d4f
CASAhmadou <amalsakho92@gmail.com>
2023-04-15 15:23:05 +0000

    debut recherche

--- lib/ui/pages/user.page.dart
+++ lib/ui/pages/user.page.dart
@@ -1,18 +1,35 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 class UserPage extends StatefulWidget {
   @override
   State<UserPage> createState() => _UserPageState();
 }
 
 class _UserPageState extends State<UserPage> {
-  var search;
+  var query;
   bool notVisible=false;
   TextEditingController textController = new TextEditingController();
+  dynamic data;
 
+  void _search(String query) {
+    String url = "https://api.github.com/search/users?q=${query}&per_page=20&page=0";
+    print(url);
+    http.get(Uri.parse(url))
+        .then((response) {
+          setState(() {
+            this.data =json.decode(response.body);
+          });
+    })
+        .catchError((err){
+      print(err);
+    });
+  }
 @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text('Users => ${search}'),),
+      appBar: AppBar(title: Text('Users => ${query}'),),
       body: Center(
         child: Column(
           children: [
@@ -25,7 +42,7 @@ class _UserPageState extends State<UserPage> {
                         obscureText: notVisible,
                         onChanged: (value){
                           setState(() {
-                            this.search=value;
+                            this.query=value;
                           });
                         },
                         controller: textController,
@@ -37,7 +54,8 @@ class _UserPageState extends State<UserPage> {
                                   notVisible=!notVisible;
                                 });
                               },
-                              icon: Icon(Icons.visibility),
+                              icon: Icon(
+                                  notVisible==true?Icons.visibility_off:Icons.visibility),
                             ),
                             contentPadding: EdgeInsets.only(left: 20),
                             counterText: '0 character',
@@ -55,11 +73,22 @@ class _UserPageState extends State<UserPage> {
                     icon: Icon(Icons.search, color: Colors.deepOrange),
                     onPressed: (){
                       setState(() {
-                        this.search = textController.text;
+                        this.query = textController.text;
+                        _search(query);
                       });
                     }
                 )
               ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: (data==null)?0:data['items'].length,
+                  itemBuilder: (context,index){
+                    return ListTile(
+                      title: Text("${data['items'][index]['login']}"),
+                    );
+                  }
+              ),
             )
           ],
         )
